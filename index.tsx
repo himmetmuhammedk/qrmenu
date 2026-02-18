4ac2be496525c8ab3e92e3641bcfb9d01955e2ce
himmetmuhammedk <himmetmuhammedk@gmail.com>
2026-02-18 13:33:59 +0300

    fix

--- index.tsx
+++ index.tsx
@@ -1,15 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// All code has been moved to index.html to support GitHub Pages static hosting without a build step.
